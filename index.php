bacb01072a16cda95d6079a66e0b743a964b1654
pico <le_testeur@hotmail.com>
2013-01-21 15:35:52 +0100

    demo TD : insertion BootStrap

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
-﻿
+<?php
+  Header('Cache-Control: no-cache');
+  Header('Pragma: no-cache');
+  date_default_timezone_set('UTC');
+?>
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title></title>
-    </head>
-    <body>
-        <form id="form1" >
-        <div>
-            Un site plein de nouveaux trucs..<br/>
-            <?echo phpinfo();?>
-        </div>
-        </form>
+  <head>
+    <title>Une page PHP</title>
+  </head>
+  <body class="container">
+    <h1>Une page PHP</h1>
+        <p><a class="btn btn-primary btn-large" href="Index.aspx">Index.aspx</a></p>
+    <p class="lead">Il est <?php $now = date('H:i:s'); echo $now; ?></p>
     </body>
 </html>
