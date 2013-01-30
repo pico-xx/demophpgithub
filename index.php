4a92855078c362bd5f14d54c27c6ba926ef8e1a8
pico <le_testeur@hotmail.com>
2013-01-30 01:38:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-  <base href="http://azpnet-----------j1.azurewebsites.net/" />
+  <base href="http://aspnet-----------j1.azurewebsites.net/" />
   <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
     <title>Une page PHP</title>
 
